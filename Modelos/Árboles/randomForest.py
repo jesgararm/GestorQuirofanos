aaf77a3ea0b47d3128615af7e403b4dfd6ed4239
Jesús GA <94868006+jesgararm@users.noreply.github.com>
2023-04-08 12:39:53 +0200

    Aplicamos el modelo de Random Forest

---
+++ Modelos/Árboles/randomForest.py
@@ -0,0 +1,60 @@
+# Imports necesarios
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+import numpy as np
+import joblib
+import matplotlib.pyplot as plt
+# Clase que implementa el modelo de Random Forest
+class RandomForest:
+    # Constructor
+    def __init__(self, X, y):
+        # División de los datos en train y test
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+        # Creación del modelo
+        self.model = RandomForestRegressor(n_estimators=100, random_state=0)
+        # Entrenamiento del modelo
+        self.model.fit(self.X_train, self.y_train)
+        self.y_pred = self.model.predict(self.X_test)
+    # Método que devuelve el error absoluto medio
+    def getMAE(self):
+        # Cálculo del error absoluto medio
+        return mean_absolute_error(self.y_test, self.y_pred)
+
+    # Método que devuelve el error cuadrático medio
+    def getMSE(self):
+        # Cálculo del error cuadrático medio
+        return mean_squared_error(self.y_test, self.y_pred)
+
+    # Método que devuelve el error cuadrático medio
+    def getRMSE(self):
+        # Cálculo del error cuadrático medio
+        return np.sqrt(mean_squared_error(self.y_test, self.y_pred))
+
+    # Método que devuelve el score del modelo
+    def getScore(self):
+        # Cálculo del score
+        return self.model.score(self.X_test, self.y_test)
+
+    # Método que devuelve el valor de la predicción
+    def getPrediction(self, X):
+        return self.model.predict(X)
+    # Método que dibuja el gráfico de predicción
+    def drawPerformance(self):
+        plt.figure()
+        plt.scatter(np.arange(self.y_test.shape[0]), self.y_test, s=20, edgecolor="black", c="darkorange", label="Real")
+        plt.plot(np.arange(self.y_test.shape[0]), self.y_pred, color="cornflowerblue", label="Predicted", linewidth=2)
+        plt.ylim(0, 400)
+        plt.yticks(np.arange(0, 400, 20))
+        plt.xlabel("data")
+        plt.ylabel("target")
+        plt.title("Random Forest Regression")
+        plt.legend()
+        plt.show()
+    # Método que guarda el modelo
+    def exportModel(self, path):
+        joblib.dump(self.model, path)
+    # Método que carga el modelo
+    def importModel(self, path):
+        self.model = joblib.load(path)
--- Modelos/Árboles/regressionTree.py
+++ Modelos/Árboles/regressionTree.py
@@ -2,7 +2,7 @@
 # Importación de librerías
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_squared_error
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import cross_val_predict
 from sklearn.model_selection import train_test_split
@@ -27,11 +27,14 @@ class RegressionTree:
         self.tree.fit(self.X_train, self.y_train)
         self.y_pred = self.predict(self.X_test)
         self.mse = mean_squared_error(self.y_test, self.y_pred)
+        self.mae = mean_absolute_error(self.y_test, self.y_pred)
         self.rmse = np.sqrt(self.mse)
         self.scores = cross_val_score(self.tree, self.X_train, self.y_train, scoring="neg_mean_squared_error", cv=10)
         self.cross_val_predictions = cross_val_predict(self.tree, self.X_train, self.y_train, cv=10)
     def predict(self, X):
         return self.tree.predict(X)
+    def getMAE(self):
+        return self.mae
     def getMSE(self):
         return self.mse
     def getRMSE(self):
