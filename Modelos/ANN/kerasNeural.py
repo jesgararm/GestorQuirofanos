011145da1d8c7df00ac7ffd116531f379916ee06
Jesús GA <94868006+jesgararm@users.noreply.github.com>
2023-04-14 02:20:01 +0200

    Creamos clase MyHyperModel

--- Modelos/ANN/kerasNeural.py
+++ Modelos/ANN/kerasNeural.py
@@ -1,5 +1,6 @@
 # Definimos modelo con Keras
 # Importamos librerias
+from os.path import normpath
 import tensorflow as tf
 from tensorflow import keras
 from keras.models import Sequential
@@ -9,44 +10,34 @@ from keras.optimizers import Adam
 from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
-# Definimos modelo
-def build_model(hp):
-    # Definimos modelo
-    model = Sequential()
-    # Definimos capas ocultas
-    for i in range(hp.Int('num_layers', 2, 20)):
-        model.add(Dense(units=hp.Int('units_' + str(i),
-                                     min_value=32,
-                                     max_value=512,
-                                     step=32),
-                        activation='relu'))
-    # Definimos capa de salida
-    model.add(Dense(1, activation='linear'))
-    # Compilamos modelo
-    model.compile(
-        optimizer=Adam(
-            hp.Choice('learning_rate', [1e-2, 1e-3, 1e-4])),
-        loss='mean_absolute_error',
-        metrics=['mean_absolute_error'])
-    return model
-def get_Tuner():
-    # Definimos tuner
-    tuner = Hyperband(build_model, objective='val_mean_absolute_error',directory='keras',project_name='gestorQuirofanos',max_epochs=10)
-    return tuner
-# Función que entrena el modelo
-def trainModel(X, Y, tuner):
-    # Entrenamos modelo
-    tuner.search(X, Y, epochs=5, validation_split=0.2)
-    # Guardamos el mejor modelo
-    best_model = tuner.get_best_models(num_models=1)[0]
-    best_model.save('keras/gestorQuirofanos/best_model.h5')
-    return best_model
+import keras_tuner
+class MyHyperModel(keras_tuner.HyperModel):
+    def build(self, hp):
+        # Definimos modelo
+        model = Sequential()
+        # Definimos capas ocultas
+        for i in range(hp.Int('num_layers', 2, 20)):
+            model.add(Dense(units=hp.Int('units_' + str(i),
+                                        min_value=32,
+                                        max_value=512,
+                                        step=32),
+                            activation='relu'))
+        # Definimos capa de salida
+        model.add(Dense(1, activation='linear'))
+        # Compilamos modelo
+        model.compile(
+            optimizer=Adam(
+                hp.Choice('learning_rate', [1e-2, 1e-3, 1e-4])),
+            loss='mean_absolute_error',
+            metrics=['mean_absolute_error'])
+        return model
 
 class KerasNeural:
     def __init__(self,X,Y):
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
-        self.tuner = get_Tuner()
-        self.model = trainModel(X_train, Y_train, self.tuner)
+        self.tuner = Hyperband(MyHyperModel(),max_epochs=10, objective='mean_absolute_error', directory="keras", project_name="gestorQuirofanos")
+        self.tuner.search(X_train, Y_train, epochs=10, validation_data=(X_test, Y_test))
+        self.model = self.tuner.get_best_models(num_models=1)[0]
         self.Y_test = Y_test
         self.Y_pred = self.model.predict(X_test)
         self.mae = mean_absolute_error(self.Y_test, self.Y_pred)
