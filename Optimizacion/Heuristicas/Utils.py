c96b7f8f380e310871596d6b4099e59cc0384916
Jesús GA <94868006+jesgararm@users.noreply.github.com>
2023-05-06 01:36:49 +0200

    Heuristica LPT

---
+++ Optimizacion/Heuristicas/Utils.py
@@ -0,0 +1,34 @@
+import random
+import numpy as np
+
+# Clase que representa un acto quirúrgico.
+# Contiene el id del acto, la duración y el id del paciente.
+class ActoQuirurgico:
+    def __init__(self, id, duracion, idPaciente, prioridad):
+        self.id = id
+        self.duracion = duracion
+        self.idPaciente = idPaciente
+        self.prioridad = prioridad
+    def getId(self):
+        return self.id
+    def getDuracion(self):
+        return self.duracion
+    def getIdPaciente(self):
+        return self.idPaciente
+    def getPrioridad(self):
+        return self.prioridad
+    
+# Clase que representa un Quirófano
+# Contiene el id del quirófano, el día y el tiempo de trabajo.
+class Quirofano:
+    def __init__(self, id, dia, tiempo):
+        self.id = id
+        self.dia = dia
+        self.tiempo = tiempo
+    def getId(self):
+        return self.id
+    def getDia(self):
+        return self.dia
+    def getTiempo(self):
+        return self.tiempo
+
