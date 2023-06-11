ecc833e11b5bc228b479b88d821c4929e2b0b0f5
Jesús GA <94868006+jesgararm@users.noreply.github.com>
2023-06-11 12:31:03 +0200

    .

--- Interfaz/src/app.py
+++ Interfaz/src/app.py
@@ -12,24 +12,36 @@ from models.entities.user import User
 # Se crea la aplicación
 app = Flask(__name__)
 db = MySQL(app)
-@app.route('/')
-def index():
-    return redirect(url_for('login'))
 
-@app.route('/login', methods=['GET', 'POST'])
+# Método de inicio de la aplicación
+@app.route("/")
+def index():
+    return redirect(url_for("login"))
+
+# Método de login de usuario
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method == 'POST':
-        user = User(request.form['inputEmail'], request.form['inputPassword'])
+    if request.method == "POST":
+        # Comprobamos si el usuario existe
+        user = User(request.form["inputEmail"], request.form["inputPassword"])
         logged_user = ModelUser.login(db, user)
         if logged_user != None:
-            pass
+            if logged_user.password:
+                return redirect(url_for("home"))
+            else:
+                flash("Contraseña incorrecta")
         else:
-            flash('Usuario no encontrado')
-            return render_template('auth/login.html')
+            # Si no existe, se muestra un mensaje de error
+            flash("Usuario no encontrado")
+            return render_template("auth/login.html")
     else:
-        return render_template('auth/login.html')
+        return render_template("auth/login.html")
 
-if __name__ == '__main__':
+# Método para el Home de un usuario
+@app.route("/home")
+def home():
+    return render_template("home.html")
+if __name__ == "__main__":
     # Se configura la aplicación
-    app.config.from_object(config['development'])
-    app.run(debug=True)
+    app.config.from_object(config["development"])
+    app.run()
